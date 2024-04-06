5d260290ad8630c7c95d6f5fbc81fc71afec7d03
Hector <hector.matias@ecam.es>
2024-04-06 16:07:26 +0200

    added localforage

--- app/logic.js
+++ app/logic.js
@@ -1,14 +1,20 @@
+import { useEffect } from 'react';
 import { useBooksData } from "./hooks/useBooksData";
 import useStore from "./store";
 
 const useLogic = () => {
   const books = useBooksData();
   const selectBook = useStore((state) => state.selectBook);
+  const initializeBooks = useStore((state) => state.initializeBooks);
   const savedBooks = useStore((state) => state.savedBooks);
   const selectedBook = useStore((state) => state.selectedBook);
   const isModalOpen = useStore((state) => state.isModalOpen);
   const deselectBook = useStore((state) => state.deselectBook);
   const toggleBookSaved = useStore((state) => state.toggleBookSaved);
+
+  useEffect(() => {
+    initializeBooks();
+  }, [initializeBooks]);
 
   const checkIsFavorite = (book) => {
     return savedBooks.some((savedBook) => savedBook.ISBN === book.ISBN);
@@ -18,8 +24,8 @@ const useLogic = () => {
     selectBook(book);
   };
 
-  const handleToggleBookSaved = (book) => {
-    toggleBookSaved(book);
+  const handleToggleBookSaved = async (book) => {
+    await toggleBookSaved(book);
   };
 
   return {
--- app/store.js
+++ app/store.js
@@ -1,53 +1,35 @@
-import { create } from "zustand";
-import { useEffect } from "react";
+import {create} from 'zustand'
+import localForage from 'localforage'
 
-// Estado inicial predeterminado
-const defaultState = { savedBooks: [], selectedBook: null, isModalOpen: false };
+localForage.config({
+  name: 'tradingproBooks',
+  storeName: 'books',
+});
 
-// Función para cargar el estado desde localStorage
-// Nota: Esta función ya no se llama directamente en la creación de la tienda.
-const loadStateFromLocalStorage = () => {
-  const savedState = localStorage.getItem("savedBooksState");
-  return savedState ? JSON.parse(savedState) : defaultState;
-};
+const useStore = create(set => ({
+  savedBooks: [],
+  initializeBooks: async () => {
+    const savedBooks = await localForage.getItem('savedBooks') || [];
+    set({ savedBooks });
+  },
+  toggleBookSaved: async (bookToToggle) => {
+    set(state => {
+      const isCurrentlySaved = state.savedBooks.some(book => book.ISBN === bookToToggle.ISBN);
+      const updatedSavedBooks = isCurrentlySaved
+        ? state.savedBooks.filter(book => book.ISBN !== bookToToggle.ISBN)
+        : [...state.savedBooks, bookToToggle];
+      
+      localForage.setItem('savedBooks', updatedSavedBooks);
 
-// Crea la tienda sin cargar inmediatamente desde localStorage
-const useStore = create((set, get) => ({
-  ...defaultState,
-  toggleBookSaved: (bookToToggle) =>
-    set((state) => {
-      const isCurrentlySaved = state.savedBooks.some((book) => book.ISBN === bookToToggle.ISBN);
-      const newState = {
-        savedBooks: isCurrentlySaved
-          ? state.savedBooks.filter((book) => book.ISBN !== bookToToggle.ISBN)
-          : [...state.savedBooks, bookToToggle],
+      return {
+        savedBooks: updatedSavedBooks,
       };
-      return newState;
-    }),
-  selectBook: (book) => {
-    set({ selectedBook: book, isModalOpen: true });
+    });
   },
-  deselectBook: () => {
-    set({ selectedBook: null, isModalOpen: false });
-  },
-  // Nueva acción para establecer el estado directamente
-  setState: (newState) => {
-    set(newState);
-  },
+  selectedBook: null,
+  isModalOpen: false,
+  selectBook: (book) => set({ selectedBook: book, isModalOpen: true }),
+  deselectBook: () => set({ selectedBook: null, isModalOpen: false }),
 }));
-
-// Hook personalizado para manejar localStorage
-export const useHydratedStore = () => {
-  const setState = useStore((state) => state.setState);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const state = loadStateFromLocalStorage();
-      setState(state);
-    }
-  }, [setState]);
-
-  return useStore;
-};
 
 export default useStore;
