83c6007b910284b6ee1cf15f3d808193926b08cb
Hector <hector.matias@ecam.es>
2024-04-06 13:53:47 +0200

    me voy a comer

--- app/store.js
+++ app/store.js
@@ -1,44 +1,53 @@
-import create from "zustand";
+import { create } from "zustand";
+import { useEffect } from "react";
 
-const saveStateToLocalStorage = (state) => {
-  localStorage.setItem("savedBooksState", JSON.stringify(state));
-};
+// Estado inicial predeterminado
+const defaultState = { savedBooks: [], selectedBook: null, isModalOpen: false };
 
+// Función para cargar el estado desde localStorage
+// Nota: Esta función ya no se llama directamente en la creación de la tienda.
 const loadStateFromLocalStorage = () => {
   const savedState = localStorage.getItem("savedBooksState");
-  return savedState
-    ? JSON.parse(savedState)
-    : { savedBooks: [], selectedBook: null, isModalOpen: false };
+  return savedState ? JSON.parse(savedState) : defaultState;
 };
 
+// Crea la tienda sin cargar inmediatamente desde localStorage
 const useStore = create((set, get) => ({
-  ...loadStateFromLocalStorage(),
+  ...defaultState,
   toggleBookSaved: (bookToToggle) =>
     set((state) => {
-      const isCurrentlySaved = state.savedBooks.some(
-        (book) => book.ISBN === bookToToggle.ISBN,
-      );
+      const isCurrentlySaved = state.savedBooks.some((book) => book.ISBN === bookToToggle.ISBN);
       const newState = {
         savedBooks: isCurrentlySaved
           ? state.savedBooks.filter((book) => book.ISBN !== bookToToggle.ISBN)
           : [...state.savedBooks, bookToToggle],
       };
-      saveStateToLocalStorage({ ...get(), ...newState });
       return newState;
     }),
   selectBook: (book) => {
-    const newState = { selectedBook: book, isModalOpen: true };
-    saveStateToLocalStorage({ ...get(), ...newState });
-    set(newState);
+    set({ selectedBook: book, isModalOpen: true });
   },
   deselectBook: () => {
-    const newState = { selectedBook: null, isModalOpen: false };
-    console.log("llega");
-    saveStateToLocalStorage({ ...get(), ...newState });
+    set({ selectedBook: null, isModalOpen: false });
+  },
+  // Nueva acción para establecer el estado directamente
+  setState: (newState) => {
     set(newState);
   },
 }));
 
- 
+// Hook personalizado para manejar localStorage
+export const useHydratedStore = () => {
+  const setState = useStore((state) => state.setState);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const state = loadStateFromLocalStorage();
+      setState(state);
+    }
+  }, [setState]);
+
+  return useStore;
+};
 
 export default useStore;
