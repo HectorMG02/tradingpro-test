74b173d49e9fbe0182f486c5e8ad18d09d09b95b
Hector <hector.matias@ecam.es>
2024-04-06 16:44:11 +0200

    broadcast system added

--- app/store.js
+++ app/store.js
@@ -1,35 +1,44 @@
-import {create} from 'zustand'
-import localForage from 'localforage'
+import create from 'zustand';
+import localForage from 'localforage';
 
 localForage.config({
-  name: 'tradingproBooks',
+  name: 'myApp',
   storeName: 'books',
 });
 
-const useStore = create(set => ({
+
+const channel = typeof window !== 'undefined' ? new BroadcastChannel('booklist_channel') : null;
+
+const useStore = create((set, get) => ({
   savedBooks: [],
   initializeBooks: async () => {
     const savedBooks = await localForage.getItem('savedBooks') || [];
     set({ savedBooks });
   },
   toggleBookSaved: async (bookToToggle) => {
-    set(state => {
-      const isCurrentlySaved = state.savedBooks.some(book => book.ISBN === bookToToggle.ISBN);
-      const updatedSavedBooks = isCurrentlySaved
-        ? state.savedBooks.filter(book => book.ISBN !== bookToToggle.ISBN)
-        : [...state.savedBooks, bookToToggle];
-      
-      localForage.setItem('savedBooks', updatedSavedBooks);
+    const { savedBooks } = get();
+    const isCurrentlySaved = savedBooks.some(book => book.ISBN === bookToToggle.ISBN);
+    const updatedSavedBooks = isCurrentlySaved
+      ? savedBooks.filter(book => book.ISBN !== bookToToggle.ISBN)
+      : [...savedBooks, bookToToggle];
 
-      return {
-        savedBooks: updatedSavedBooks,
-      };
-    });
+    await localForage.setItem('savedBooks', updatedSavedBooks);
+    set({ savedBooks: updatedSavedBooks });
+
+    channel?.postMessage('update');
   },
   selectedBook: null,
   isModalOpen: false,
   selectBook: (book) => set({ selectedBook: book, isModalOpen: true }),
   deselectBook: () => set({ selectedBook: null, isModalOpen: false }),
 }));
+
+
+channel?.addEventListener('message', async (event) => {
+  if (event.data === 'update') {
+    const savedBooks = await localForage.getItem('savedBooks');
+    useStore.setState({ savedBooks });
+  }
+});
 
 export default useStore;
