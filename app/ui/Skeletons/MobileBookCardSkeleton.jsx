7c7ce8b66c6222f5442881ca6a0ce39276a5db72
Hector <hector.matias@ecam.es>
2024-04-07 12:16:00 +0200

    repsonsive book card skeleton

---
+++ app/ui/Skeletons/MobileBookCardSkeleton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MobileBookCardSkeleton = () => {
+    return <div className="h-[200px] w-full mb-4 bg-gray-300 rounded"></div>;
+};
+
+export default MobileBookCardSkeleton;
