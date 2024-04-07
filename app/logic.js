2016025dcb39df91309c9d7d35642cd05aece9f1
Hector <hector.matias@ecam.es>
2024-04-07 12:20:53 +0200

    detect if is a phone with tailwind

--- app/logic.js
+++ app/logic.js
@@ -54,7 +54,7 @@ const useLogic = () => {
     handleToggleBookSaved,
     genres,
     handleGenreChange,
-    selectedGenre,
+    selectedGenre
   };
 };
 
--- app/ui/Skeletons/DashboardSkeleton.jsx
+++ app/ui/Skeletons/DashboardSkeleton.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import BookCardSkeleton from './BookCardSkeleton';
 import MobileBookCardSkeleton from './MobileBookCardSkeleton';
@@ -5,8 +7,6 @@ import SavedBookCardSkeleton from './SavedBookCardSkeleton';
 import GenreSelectorSkeleton from './GenreSelectorSkeleton';
 
 const DashboardSkeleton = () => {
-    const isMobile = window.innerWidth < 768;
-
     return (
         <div>
             <div className="bg-gray-500">
@@ -25,15 +25,16 @@ const DashboardSkeleton = () => {
                 <div className="h-2 mx-0 mb-5 px-5 pt-8 bg-gray-300 rounded w-36"></div>
                 <GenreSelectorSkeleton />
                 <div className="md:grid md:grid-cols-3 gap-4">
-                    {new Array(9)
-                        .fill(0)
-                        .map((_, index) =>
-                            isMobile ? (
+                    {new Array(9).fill(0).map((_, index) => (
+                        <div key={index}>
+                            <div className="hidden md:block">
+                                <BookCardSkeleton />
+                            </div>
+                            <div className="md:hidden">
                                 <MobileBookCardSkeleton key={index} />
-                            ) : (
-                                <BookCardSkeleton key={index} />
-                            )
-                        )}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
